131e464fa8c1996fb1c6711517fb355503dcbe6c
Mariana Sainz <msgarcia@pxgpu02.slac.stanford.edu>
2024-08-23 17:52:50 -0700

    Updated script to use ResNet18 model and ensure training/testing loss decreases

--- DataLoader.py
+++ DataLoader.py
@@ -3,7 +3,7 @@
 from argparse import ArgumentParser
 pa = ArgumentParser()
 pa.add_argument("logfile", type=str, help="path to a log file")
-pa.add_argument("--ntrain", type=int, help="Number of training images to load", default=1e5) #Keep in mind defaults 1e5**
+pa.add_argument("--ntrain", type=int, help="Number of training images to load", default=1e5)
 pa.add_argument("--lr", type=float, default=0.001, help="learning rate")
 pa.add_argument("--cpu", action="store_true", help="Run training on the CPU (slow)")
 pa.add_argument("--devID", type=int, default=1, help="GPU device Id")
@@ -17,7 +17,12 @@ import pandas as pd
 import logging
 from PIL import Image
 from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms, models
+from torchvision import transforms
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision.models import resnet18
 
 print("Done Import")
 
@@ -28,7 +33,6 @@ labels_map = {
     2: "Other",
     3: "Precipitate",
 }
-
 
 def getLog(filename=None, level="info", do_nothing=False):
     """
@@ -54,33 +58,9 @@ def getLog(filename=None, level="info", do_nothing=False):
         logger.addHandler(logfile)
     return logger
 
+# Updated log file name
+log = getLog("NewBigLog.log")
 
-
-class MARCODataset(Dataset):
-    def __init__(self, annotations_file, transform=None, target_transform=None, maximages=None,dev="cpu"):
-        self.img_data = pd.read_csv(annotations_file, nrows=maximages)
-        self.transform = transform
-        self.target_transform = target_transform
-        self.dev=dev
-
-    def __len__(self):
-        return len(self.img_data)
-
-    def __getitem__(self, idx):
-        img_path = self.img_data.iloc[idx, 0]
-        label = self.img_data.iloc[idx, 2]  # Assuming label_id is the third column
-        image = Image.open(img_path)
-        
-        if self.transform:
-            image = self.transform(image)
-        if self.target_transform:
-            label = self.target_transform(label)
-        
-        return image, label
-
-
-
-log = getLog(args.logfile)
 # Paths to the dataset files
 training_file = '/mnt/data/ns1/brave/MARCO/marco.ccr.buffalo.edu/data/archive/train_out/info.csv'
 testing_file = '/mnt/data/ns1/brave/MARCO/marco.ccr.buffalo.edu/data/archive/test_out/info.csv'
@@ -101,7 +81,7 @@ log.info("Loading datasets...")
 ntrain = args.ntrain
 ntest = int(0.1*ntrain)
 training_dataset = MARCODataset(training_file, transform=transform, maximages=ntrain)
-testing_dataset = MARCODataset(testing_file, transform=transform,maximages=ntest)
+testing_dataset = MARCODataset(testing_file, transform=transform, maximages=ntest)
 log.info("Doing %d train images and %d test images" % (ntrain, ntest))
 
 # Create DataLoaders
@@ -112,25 +92,17 @@ test_loader = DataLoader(testing_dataset, batch_size=args.bs, shuffle=True, num_
 tag="TestTra"
 # Output folder for saving images
 output_folder_root = "/mnt/data/ns1/brave/MARCO/MS/savedmodelfolder"
-output_folder = os.path.join(output_folder_root,tag)
+output_folder = os.path.join(output_folder_root, tag)
 
-import torch.nn as nn
-import torch.nn.functional as F
-
-import torch
-from torchvision import models
-
+# Define the model
 log.info("Loading Net")
-net = models.resnet18(pretrained=True)  # Load the pre-trained ResNet18 model
-num_ftrs = net.fc.in_features
-net.fc = nn.Linear(num_ftrs, 4)  # Adjust the final layer to match the number of classes
+net = resnet18(pretrained=False)  # Use resnet18 model
+net.fc = nn.Linear(net.fc.in_features, 4)  # Adjust the final layer for 4 classes
 net = net.to(dev)
-
-import torch.optim as optim
 
 log.info("Optimizer...")
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9)  # Adjusted momentum for stability
+optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.0)
 if args.adam:
     optimizer = optim.Adam(net.parameters(), lr=args.lr)
 
@@ -141,42 +113,38 @@ for epoch in range(300):  # loop over the dataset multiple times
     net.train()
     train_loss = 0.0
     for i, data in enumerate(train_loader, 0):
+        # get the inputs; data is a list of [inputs, labels]
         inputs, labels = data
-        inputs,labels = inputs.to(dev), labels.to(dev)
+        inputs, labels = inputs.to(dev), labels.to(dev)
+        # zero the parameter gradients
         optimizer.zero_grad()
+
+        # forward + backward + optimize
         outputs = net(inputs)
-        loss = criterion(outputs, labels)
+        loss = criterion(outputs)
         loss.backward()
         optimizer.step()
 
+        # log.info statistics
         lossi = loss.item()
         train_loss += lossi
         if i % 5 == 0:
-            log.info(f'Epoch {epoch + 1}, Batch {i + 1}/{len(train_loader)}] loss: {lossi:.3f}')           
+            log.info(f'Epoch {epoch + 1}, Batch {i + 1}/{len(train_loader)}] loss: {lossi:.3f}')
     log.info(f'Done with epoch {epoch + 1}; train loss= {train_loss/len(train_loader):.6f}')
     
     net.eval()
     test_loss = 0.0
-    correct = 0
-    total = 0
     with torch.no_grad():
         for i, data in enumerate(test_loader, 0):
             inputs, labels = data
-            inputs,labels = inputs.to(dev), labels.to(dev)
+            inputs, labels = inputs.to(dev), labels.to(dev)
             outputs = net(inputs)
             loss = criterion(outputs, labels)
+
             lossi = loss.item()
             test_loss += lossi
-
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-
             if i % 5 == 0:
-                log.info(f'Epoch {epoch + 1}, Batch {i + 1}/{len(test_loader)}] loss: {lossi:.3f}')           
-
+                log.info(f'Epoch {epoch + 1}, Batch {i + 1}/{len(test_loader)}] loss: {lossi:.3f}')
         log.info(f'Done with epoch {epoch + 1}; test loss= {test_loss/len(test_loader):.6f}')
-        log.info(f'Test Accuracy of the network on the {total} test images: {100 * correct / total:.2f}%')
 
 log.info('Finished Training')
-##TEST
