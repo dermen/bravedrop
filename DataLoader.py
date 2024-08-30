9cb26534df76e797d81cbb298940602b6dec8fd9
Mariana Sainz <msgarcia@pxgpu02.slac.stanford.edu>
2024-08-29 20:28:28 -0700

    Resnets and GPUs Updates but testting it out again

--- DataLoader.py
+++ DataLoader.py
@@ -60,7 +60,7 @@ def getLog(filename=None, level="info", do_nothing=False):
     return logger
 
 # Initialize logging
-log = getLog('RS50Log.log')
+log = getLog(args.logfile)
 # Paths to the dataset files
 training_file = '/mnt/data/ns1/brave/MARCO/marco.ccr.buffalo.edu/data/archive/train_out/info.csv'
 testing_file = '/mnt/data/ns1/brave/MARCO/marco.ccr.buffalo.edu/data/archive/test_out/info.csv'
@@ -70,12 +70,59 @@ if args.cpu:
     dev = "cpu"
 log.info(f"Running model on device {dev}.")
 
+# Define the MARCODataset class
+class MARCODataset(Dataset):
+    def __init__(self, annotations_file, use_complex_transform=True, target_transform=None, maximages=-1, dev="cpu"):
+        if maximages == -1:
+            maximages = None
+        self.img_data = pd.read_csv(annotations_file, nrows=maximages)
+        self.target_transform = target_transform
+        self.dev = dev
+        self.use_complex_transform = use_complex_transform
+
+    def __len__(self):
+        return len(self.img_data)
+
+    def __getitem__(self, idx):
+        img_path = self.img_data.iloc[idx, 0]
+        label = self.img_data.iloc[idx, 2]  # Assuming label_id is the third column
+        image = Image.open(img_path)
+        
+        if self.use_complex_transform:
+            transform = self.complex_preprocess()
+        else:
+            transform = self.simple_preprocess()
+
+        image = transform(image)
+
+        if self.target_transform:
+            label = self.target_transform(label)
+        
+        return image, label
+
+    def simple_preprocess(self):
+        transform = transforms.Compose([
+            transforms.Resize((600, 600)),  # Resize to 600 x 600 or any consistent size
+            transforms.ToTensor()  # Convert image to tensor
+        ])
+        return transform
+
+    def complex_preprocess(self):
+        transform = transforms.Compose([
+            transforms.Resize((600, 600)),  # Resize to 600 x 600 or any consistent size
+            transforms.RandomRotation(degrees=90),  # Random rotation
+            transforms.RandomHorizontalFlip(),  # Random horizontal flip
+            transforms.RandomVerticalFlip(),  # Random vertical flip
+            transforms.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5)),  # Random Gaussian blur
+            transforms.ToTensor()  # Convert image to tensor
+        ])
+        return transform
+
 # Create datasets
 log.info("Loading datasets...")
 ntrain = args.ntrain
 ntest = int(0.1 * ntrain)
 
-# Updated to match the class definition
 training_dataset = MARCODataset(training_file, use_complex_transform=True, maximages=ntrain, dev=dev)
 testing_dataset = MARCODataset(testing_file, use_complex_transform=True, maximages=ntest, dev=dev)
 log.info("Doing %d train images and %d test images" % (ntrain, ntest))
